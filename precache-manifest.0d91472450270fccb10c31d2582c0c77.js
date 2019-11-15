242770f41ac6f38586a24ab6230c30fa642f21bb
Avijit Mandal <avijit.ce15@gmail.com>
2019-11-15 12:11:17 +0530

    Updates

---
+++ precache-manifest.0d91472450270fccb10c31d2582c0c77.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbfbe182cc3870aea004bba6559dabf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71c3b9d28a1d19cc328",
+    "url": "/static/css/main.09e29848.chunk.css"
+  },
+  {
+    "revision": "55a71a0ceb0e2f9b9c11",
+    "url": "/static/js/2.60304d26.chunk.js"
+  },
+  {
+    "revision": "b71c3b9d28a1d19cc328",
+    "url": "/static/js/main.9b089fd3.chunk.js"
+  },
+  {
+    "revision": "c7fc56e31c0aa8014aa9",
+    "url": "/static/js/runtime-main.34065fd5.js"
+  },
+  {
+    "revision": "05f20d700aba922527a65adfdabc3cd6",
+    "url": "/static/media/logo.05f20d70.svg"
+  },
+  {
+    "revision": "8764b3dc0548ee91bd1016fb8d8a2d9c",
+    "url": "/static/media/user-1.8764b3dc.jpg"
+  },
+  {
+    "revision": "02164aef2e6d3699e86eb2e196e27793",
+    "url": "/static/media/user-2.02164aef.jpg"
+  },
+  {
+    "revision": "f5e39d5fa0b4704abe0cf0f2eab905ff",
+    "url": "/static/media/user-3.f5e39d5f.jpg"
+  },
+  {
+    "revision": "2cb1f2df693f88aa2029bfd75fbbd2d9",
+    "url": "/static/media/user-4.2cb1f2df.jpg"
+  },
+  {
+    "revision": "f6675c7c9f0d20c92c01b79f7d30a92f",
+    "url": "/static/media/user-5.f6675c7c.jpg"
+  },
+  {
+    "revision": "0708276af8157f64cbaca923b79724fd",
+    "url": "/static/media/user-6.0708276a.jpg"
+  },
+  {
+    "revision": "4ce423d3db8f42293ffb2bbb56b5a1e1",
+    "url": "/static/media/user-7.4ce423d3.jpg"
+  },
+  {
+    "revision": "ee8a21df560444aa2c10009ada1d6ff5",
+    "url": "/static/media/user-8.ee8a21df.jpg"
+  }
+]);
